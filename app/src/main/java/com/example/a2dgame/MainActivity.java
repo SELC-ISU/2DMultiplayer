c859df9f9e37cb338437fd9962f85728e7a7b4b9
DevinM <milligandm@hotmail.com>
2020-02-11 10:55:34 -0600

    Added a Bluetooth Activity class that will be used to do all the bluetooth commands and stuff enableing, establishing dsicovery and making your self discoverable

--- app/src/main/java/com/example/a2dgame/MainActivity.java
+++ app/src/main/java/com/example/a2dgame/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.a2dgame;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView MainText = (TextView)findViewById(R.id.txtMain);
+    private BlutoothActivity activity1 = new BlutoothActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activity1.enableBT();
+
     }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        activity1.destroyConnection();
+
+    }
+
 }
