87f139a5dd48dc5187857266b2c2b08317758b30
DevinM <milligandm@hotmail.com>
2020-02-24 18:10:51 -0600

    made a simple interaction app to start a bluetooth connection as a host server or client server, but the client for now freezes when looking for connection and nothing is done with the connection

--- app/src/main/java/com/example/a2dgame/MainActivity.java
+++ app/src/main/java/com/example/a2dgame/MainActivity.java
@@ -2,30 +2,157 @@ package com.example.a2dgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Set;
 
-    private BlutoothActivity activity1 = new BlutoothActivity();
-    TextView MainText;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    TextView mainText;
+    public final int ENABLE_BT_REQUEST = 1;
+    public final int ENABLE_DISCOVERABILITY_DURATION = 180;
+    public final int ENABLE_DISCOVERABILITY = 2;
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    AcceptThread acceptThread;
+    ConnectThread connectThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
+        mainText = (TextView)findViewById(R.id.txtMain);
 
-        MainText = (TextView)findViewById(R.id.txtMain);
-        MainText.setText(Integer.toString(activity1.enableBT()));
+
+
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, ENABLE_BT_REQUEST);
+        }
+
+        Button btnEnDiscov = (Button) findViewById(R.id.btnClient);
+        btnEnDiscov.setOnClickListener(this); // calling onClick() method
+
+        Button btnMakeDiscov = (Button) findViewById(R.id.btnServer);
+        btnMakeDiscov.setOnClickListener(this);
+
+        Button btnOff = (Button) findViewById(R.id.btnOff);
+        btnOff.setOnClickListener(this);
+
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btnClient:
+
+                acceptThread = new AcceptThread(bluetoothAdapter);
+                acceptThread.run();
+
+                break;
+
+
+            case R.id.btnServer:
+
+                commenceDiscovery(true);
+
+                break;
+
+            case R.id.btnOff:
+
+                commenceDiscovery(false);
+
+                break;
+
+
+            default:
+                break;
+        }
+
+    }
+
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
 
-       // activity1.destroyConnection();
+
 
     }
+
+    public void makeDiscoverable(){
+
+        Intent discoverableIntent =
+                new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, ENABLE_DISCOVERABILITY_DURATION);
+        startActivityForResult(discoverableIntent, ENABLE_DISCOVERABILITY);
+
+    }
+    public void commenceDiscovery(boolean a){
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+       /* if (pairedDevices.size() > 0) {
+            a = false;
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                connectThread = new ConnectThread(device);
+            }
+        }
+        else {*/
+            if (a)
+                bluetoothAdapter.startDiscovery();
+            mainText = (TextView)findViewById(R.id.txtMain);
+            mainText.setText("here");
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(receiver, filter);
+
+            if (!a)
+                bluetoothAdapter.cancelDiscovery(); //This turns off discovery because it uses
+            //a lot of resources but discover is said to
+            //go for 12 seconds with sacnning period so if
+            //necessary add a delay timer here
+        }
+   // }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+
+                bluetoothAdapter.cancelDiscovery();
+
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+
+                mainText = (TextView)findViewById(R.id.txtMain);
+                mainText.setText(deviceName);
+
+                connectThread = new ConnectThread(device);
+            }
+        }
+    };
+
 
 }
