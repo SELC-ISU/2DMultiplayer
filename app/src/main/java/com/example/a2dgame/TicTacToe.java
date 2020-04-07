a62e11755601c6139f4a897798e97a50f765b349
aswen2000 <aswen2000@gmail.com>
2020-04-07 09:48:38 -0500

    working

--- app/src/main/java/com/example/a2dgame/TicTacToe.java
+++ app/src/main/java/com/example/a2dgame/TicTacToe.java
@@ -1,12 +1,44 @@
 package com.example.a2dgame;
 
 import android.content.Context;
+import android.widget.ImageView;
+import android.widget.*;
+import java.util.Scanner;
+import android.view.*;
 
-public class TicTacToe extends Thread{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TicTacToe extends AppCompatActivity, Thread {
 
     private Context context;
     boolean doublePlayer;
     boolean opponentTurn;
+    char [][] tracker = new char[3][3];
+    int cell;
+    int cellRow;
+    int cellColumn;
+    String symbol;
+
+    private ImageView x1;
+    private ImageView x2;
+    private ImageView x3;
+    private ImageView x4;
+    private ImageView x5;
+    private ImageView x6;
+    private ImageView x7;
+    private ImageView x8;
+    private ImageView x9;
+
+    private ImageView o1;
+    private ImageView o2;
+    private ImageView o3;
+    private ImageView o4;
+    private ImageView o5;
+    private ImageView o6;
+    private ImageView o7;
+    private ImageView o8;
+    private ImageView o9;
+
 
     public TicTacToe(Context context){  //this will be used in MainActivity like, TicTacToe ttt = new TicTacToe(MainActivity.this);
         this.context = context;
@@ -19,6 +51,13 @@ public class TicTacToe extends Thread{
             while (((MainActivity) context).newGameMessage == false) ;
             ((MainActivity) context).newGameMessage = false;
             String message = ((MainActivity) context).gameMessage;
+            Scanner sc = new Scanner(message);
+            symbol = sc.next();
+            cellRow = sc.nextInt();
+            cellColumn = sc.nextInt();
+            cell = toCellNumber(cellRow, cellColumn);
+
+
 
         }
         if(doublePlayer == true && opponentTurn == false) {
@@ -34,4 +73,40 @@ public class TicTacToe extends Thread{
         String msg = "";
         return msg;
     }
+
+    /**converts the coordinates from the incoming message into the cell number
+        will hopefully make determining which cell image to change easier
+     */
+    private static int toCellNumber(int row, int column){
+        int tempCell = -1;
+
+        if(row == 0){
+            if(column == 0){
+                tempCell = 1;
+            }else if(column == 1){
+                tempCell = 2;
+            }else if(column == 2){
+                tempCell = 3;
+            }
+        }else if(row == 1){
+            if(column == 0){
+                tempCell = 4;
+            }else if(column == 1){
+                tempCell = 5;
+            }else if(column == 2){
+                tempCell = 6;
+            }
+        }else if(row == 2){
+            if(column == 0){
+                tempCell = 7;
+            }else if(column == 1){
+                tempCell = 8;
+            }else if(column == 2){
+                tempCell = 9;
+            }
+        }
+
+        return tempCell;
+    }
+
 }
